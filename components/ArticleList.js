097297cfbcbb09cc5ccf90c26a46a67f6b903035
Justin Ochoa <just.och92@gmail.com>
2021-06-19 12:29:13 -0400

    messing around with next.js

---
+++ components/ArticleList.js
@@ -0,0 +1,13 @@
+import articleStyles from "../styles/Article.module.css";
+import ArticleItem from "./ArticleItem";
+const ArticleList = ({ articles }) => {
+  return (
+    <div className={articleStyles.grid}>
+      {articles.map((article, index) => {
+        return <ArticleItem key={index} article={article} />;
+      })}
+    </div>
+  );
+};
+
+export default ArticleList;
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Meta from "../components/Meta";
+
+const about = () => {
+  return (
+    <div>
+      <Meta />
+      About
+    </div>
+  );
+};
+
+export default about;
